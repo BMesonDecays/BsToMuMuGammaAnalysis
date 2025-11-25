70304d76eeca5519bf3066f816c89df7066f3e6c
Szymon <szymon.slawinski02@gmail.com>
2025-11-25 18:30:43 +0100

    changes to crab config directories

--- RadiativeAnalysis/crab/makeFileList.py
+++
@@ -1,7 +0,0 @@
-import glob
-
-
-
-with open('fileList.txt', 'w') as outFile:
-    for f in glob.glob('/eos/cms/store/group/phys_bphys/privateMC_ForBsMMGAnalysis/TrackingVertexing/Private_BsToMuMuGamma_MCTunesRun3ECM13p6TeV_09_07_2025/*/*/*/*.root'):
-        outFile.write(f.replace('/eos/cms', '') + '\n')
