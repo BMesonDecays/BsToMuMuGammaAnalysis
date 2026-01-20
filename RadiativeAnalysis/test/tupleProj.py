e9e4d460a38d65c43975de3b5c50fa70944e545f
Pawel <p.sajdak@student.uw.edu.pl>
2026-01-20 20:33:45 +0100

    new histos

--- RadiativeAnalysis/test/tupleProj.py
+++ RadiativeAnalysis/test/tupleProj.py
@@ -7,7 +7,7 @@ import numpy as np
 datasetName = "24BCD_full_BsToJpsiGamma_FL_dR"
 
 # Prepare a directory for the output histograms
-dirPath = 'tupleProjections/'+datasetName+'/cut5'
+dirPath = 'tupleProjections/'+datasetName+'/cut4_range'
 if not os.access(dirPath[:-5], os.F_OK):    # '/cut?' has 5 chars
     os.mkdir(dirPath[:-5])
 if os.access(dirPath, os.F_OK):
@@ -30,7 +30,7 @@ cutList.append(r.TCut("M_dimuonCut","TMath::Abs(M_dimuon - 3.0969) < 0.04"))    
 cutList.append(r.TCut("deltaR_dimuon_photonCut","deltaR_dimuon_photon < 0.4"))#&& deltaR_dimuon_photon > 0.1"))  #based on GenMatched tuple
 cutList.append(r.TCut("flightLengthCut","minFlightPath > 0.15"))
 #cutList.append(r.TCut("maxProb_MuMuNotGammaCut","maxProb_MuMuNotGamma < 0.9"))
-cutList.append(r.TCut("minPCA_distanceCut","minPCA_distance < 0.01"))
+#cutList.append(r.TCut("minPCA_distanceCut","minPCA_distance < 0.01"))
 
 totalCut = r.TCut()
 for cut in cutList:
@@ -59,7 +59,7 @@ binInfo = {
     branchNames[2] : (100,-3.,3.),
     branchNames[3] : (100,0.,0.5),
     branchNames[4] : (100,0.,0.05),
-    branchNames[5] : (100,3.,10.),
+    branchNames[5] : (100,4.4,6.4),
     branchNames[6] : (100,0.,10.),
     branchNames[7] : (100,0.,1.0),
     branchNames[8] : (10,0.,5.),
