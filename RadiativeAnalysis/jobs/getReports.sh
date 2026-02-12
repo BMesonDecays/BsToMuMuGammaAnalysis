f15620ce7550f1a2de728691e57672f218ee9e2b
Pawel <p.sajdak@student.uw.edu.pl>
2026-02-12 18:16:27 +0100

    moved old files to archive

--- RadiativeAnalysis/jobs/getReports.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-tag='FL_dR_resub'
-run='24D'
-
-for i in {0..7} ; do
-    crab report --dir ${run}_${tag}_BsToJpsiGamma/crab_${tag}_dset${i} --outputdir reports/${tag}/${run}${i}
-done
