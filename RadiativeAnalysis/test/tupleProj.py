65735e820e08a76be4e45a52e12df8f271d79b80
Pawel <p.sajdak@student.uw.edu.pl>
2026-03-30 21:49:59 +0200

    projections of JpsiGEndMarch

--- RadiativeAnalysis/test/tupleProj.py
+++ RadiativeAnalysis/test/tupleProj.py
@@ -4,18 +4,15 @@ import sys
 import os
 import numpy as np
 
-datasetName = "24fullB_I_JpsiMassG_BsToJpsiGamma_cut1"
+analysisTag = "JpsiGEndMarch"
 tupleName = "tOut"
-#'''
+
 # Prepare a directory for the output histograms
-dirPath = 'tupleProjections/'+tupleName+'/'+datasetName+'/Rcut4_log'
-if not os.access(dirPath[:-5], os.F_OK):    # '/cut?' has 5 chars
-    os.mkdir(dirPath[:-5])
+dirPath = 'tupleProjections/'+analysisTag+'/cut_5'
 os.mkdir(dirPath)
-#'''
 
 # Get the tuple
-tupleFile = r.TFile("./outputData/"+datasetName+".root","READ")
+tupleFile = r.TFile("./outputData/24allBtoI_cut2_"+analysisTag+".root","READ")
 ntuple = tupleFile.Get(tupleName)
 
 # Make a list with branch names
@@ -26,83 +23,77 @@ for branch in ntuple.GetListOfBranches():
 # Define the cuts
 cutList = []
 
-
-cutList.append(r.TCut("muonsKalmanVxProbCut","muonsKalmanVxProb > 0.1"))
-cutList.append(r.TCut("lXY_twoMuons_PV_signCut","lXY_twoMuons_PV_sign > 3.0"))
-cutList.append(r.TCut("dR_photon_twoMuonsCut","dR_photon_twoMuons > 0.05 && dR_photon_twoMuons < 0.4"))
-cutList.append(r.TCut("twoMuonsPhotonMassCut","TMath::Abs(twoMuonsPhotonMass - 5.367) < 1.5"))
-cutList.append(r.TCut("cosPointingAngle_twoMuonsCut","cosPointingAngle_twoMuons > 0.95"))
-cutList.append(r.TCut("lXY_twoMuons_bSpotCut","lXY_twoMuons_bSpot > 0.1"))
-cutList.append(r.TCut("maxMuonsCompCut","maxMuonsComp < 0.1"))
-
-
-'''
-cutList.append(r.TCut("fittedJpsiVxProbCut","fittedJpsiVxProb > 0.1"))
-cutList.append(r.TCut("lXY_fittedJpsi_PVCut","lXY_fittedJpsi_PV > 0.1"))
-cutList.append(r.TCut("BsXYlifetimeCut","BsXYlifetime < 9.0"))
-
-cutList.append(r.TCut("M_dimuonCut","TMath::Abs(M_dimuon - 3.0969) < 0.04"))    #Jpsi mass constraint
-#cutList.append(r.TCut("commonMuonVrtxProbCut","ProbOfCommonMuonVertex > 0.1"))
-cutList.append(r.TCut("deltaR_dimuon_photonCut","deltaR_dimuon_photon < 0.4"))#&& deltaR_dimuon_photon > 0.1"))  #based on GenMatched tuple
-cutList.append(r.TCut("flightLengthCut","minFlightPath > 0.15"))
-cutList.append(r.TCut("maxProb_MuMuNotGammaCut","maxProb_MuMuNotGamma < 0.1"))
-cutList.append(r.TCut("minPCA_distanceCut","minPCA_distance < 0.01"))
-'''
+cutList.append(r.TCut("muonIdCut","muon1Id > 0.7 && muon2Id > 0.7"))
+cutList.append(r.TCut("fittedDimuonVertexProbCut","fittedDimuonVertexProb > 0.3"))
+cutList.append(r.TCut("maxMuonsVertexCompCut","maxMuonsVertexComp < 0.1"))
+cutList.append(r.TCut("dR_photonFittedDimuonCut","dR_photonFittedDimuon < 0.4 && dR_photonFittedDimuon > 0.14"))
+cutList.append(r.TCut("fittedDimuonMassCut","TMath::Abs(fittedDimuonMass - 3.097) < 0.06"))
+cutList.append(r.TCut("lXY_fittedDimuon_bSpot_sigCut","lXY_fittedDimuon_bSpot_sig > 10.0"))
+cutList.append(r.TCut("cosAngleBsBSpot2DCut","cosAngleBsBSpot2D > 0.95"))
 
 totalCut = r.TCut()
 for cut in cutList:
     totalCut += cut
 totalCut.Print()
 
-
 with open(dirPath+'/cuts.txt','w') as of:
     print(totalCut.GetTitle(), file=of)
 
 # Define the binnings
 binInfo = {
-    branchNames[0] : (100,2.8,3.4),
-    branchNames[1] : (100,0.,1.),
+    branchNames[0] : (100,0.0,1.0),
+    branchNames[1] : (100,2.8,3.4),
     branchNames[2] : (100,0.,1.),
-    branchNames[3] : (100,0.,1.),
-    branchNames[4] : (100,0.,2.),
-    branchNames[5] : (100,0.,2.),
-    branchNames[6] : (100,0.,0.6),
-    branchNames[7] : (100,0.,0.6),
-    branchNames[8] : (200,4.4,6.4),
-    branchNames[9] : (100,4.4,6.4),
-    branchNames[10] : (100,0.,2.),
-    branchNames[11] : (100,0.,2.),
-    branchNames[12] : (100,0.,300.),
-    branchNames[13] : (100,0.,300.),
+    branchNames[3] : (100,0.,10.),
+    branchNames[4] : (100,0.,150.),
+    branchNames[5] : (100,0.,0.6),
+    branchNames[6] : (100,-3.0,3.0),
+    branchNames[7] : (100,0.,200.),
+    branchNames[8] : (100,0.9,1.01),
+    branchNames[9] : (100,3.5,7.5),
+    branchNames[10] : (100,3.5,7.5),
+    branchNames[11] : (100,-1.0,1.0),
+    branchNames[12] : (100,-1.0,1.0),
+    branchNames[13] : (100,0.,10.),
     branchNames[14] : (100,0.,10.),
-    branchNames[15] : (100,0.,10.),
-    branchNames[16] : (100,-1.,1.),
-    branchNames[17] : (100,-1.,1.),
-    branchNames[18] : (100,0.,100.),
-    branchNames[19] : (100,0.,100.)
+    branchNames[15] : (100,-1.0,1.0),
+    branchNames[16] : (100,-1.0,1.0),
+    branchNames[17] : (100,0.,50.),
+    branchNames[18] : (100,0.,50.),
+    branchNames[19] : (100,0.,1.),
+    branchNames[20] : (100,0.,1.),
+    branchNames[21] : (100,0.,1.),
+    branchNames[22] : (100,0.,1.)
 }
+
+# For "Mod" histos, removes entries without modScale
+totalCutMod = totalCut + r.TCut("modScaleCut","modScale > 0.0")
 
 # Create and fill the histograms
 histoList = []
-for bname in branchNames:
+for bname in branchNames[:-2]:
     binning = binInfo[bname]
     histo = r.TH1D("h"+bname,bname, binning[0],binning[1],binning[2])
-    #histo.SetAxisRange(7.e2,5.e4,"Y")
-    ntuple.Project("h"+bname,bname, str(totalCut))
+    if ("Mod" in bname):
+        ntuple.Project("h"+bname,bname, str(totalCutMod))
+    else:
+        ntuple.Project("h"+bname,bname, str(totalCut))
     histo.SetDirectory(0)
     histoList.append(histo)
 
 tupleFile.Close()
 
+'''
 # save selected histograms
 outFile = r.TFile("hMassAfterCompR.root",'UPDATE')
 histoList[8].Write("hTwoMuonsphotonMassCut4")
 outFile.Close()
+'''
 
 # Draw histograms and save the images
 for histo in histoList:
     canvas = r.TCanvas("c"+str(histo.GetTitle()))
-    canvas.SetLogy(1)
+    #canvas.SetLogy(1)
     canvas.cd()
     histo.Draw()
     canvas.Print(dirPath+'/'+str(histo.GetName())+".pdf")
@@ -116,7 +107,6 @@ newNtuple = ntuple.CopyTree(str(totalCut))
 newNtuple.Write()
 outputFile.Close()
 tupleFile.Close()
-
 
 
 '''
--- RadiativeAnalysis/test/tupleProjMC.py
+++ RadiativeAnalysis/test/tupleProjMC.py
@@ -4,15 +4,15 @@ import sys
 import os
 import numpy as np
 
-datasetName = "JpsiMassG_BsToJpsiGamma_MC_hlt"
+analysisTag = "JpsiGEndMarch_MC"
 tupleName = "tOut"
 
 # Prepare a directory for the output histograms
-dirPath = 'tupleProjections/'+datasetName+'/cut4_log_hlt'
+dirPath = 'tupleProjections/'+analysisTag+'/cut_t3'
 os.mkdir(dirPath)
 
 # Get the tuple
-tupleFile = r.TFile("./outputData/"+datasetName+".root","READ")
+tupleFile = r.TFile("./outputData/"+analysisTag+".root","READ")
 ntuple = tupleFile.Get(tupleName)
 
 # Make a list with branch names
@@ -23,18 +23,12 @@ for branch in ntuple.GetListOfBranches():
 # Define the cuts
 cutList = []
 
-cutList.append(r.TCut("muonsKalmanVxProbCut","muonsKalmanVxProb > 0.1"))
-cutList.append(r.TCut("lXY_twoMuons_PV_signCut","lXY_twoMuons_PV_sign > 3.0"))
-cutList.append(r.TCut("dR_photon_twoMuonsCut","dR_photon_twoMuons > 0.05 && dR_photon_twoMuons < 0.4"))
-cutList.append(r.TCut("twoMuonsPhotonMassCut","TMath::Abs(twoMuonsPhotonMass - 5.367) < 1.5"))
-cutList.append(r.TCut("cosPointingAngle_twoMuonsCut","cosPointingAngle_twoMuons > 0.95"))
-cutList.append(r.TCut("lXY_twoMuons_bSpotCut","lXY_twoMuons_bSpot > 0.1"))
-cutList.append(r.TCut("maxMuonsCompCut","maxMuonsComp < 0.1"))
-
+cutList.append(r.TCut("muonIdCut","muon1Id > 0.7 && muon2Id > 0.7"))
+cutList.append(r.TCut("fittedDimuonVertexProbCut","fittedDimuonVertexProb > 0.1"))
+cutList.append(r.TCut("maxMuonsVertexCompCut","maxMuonsVertexComp < 0.1"))
 
 # trigger cuts
-cutList.append(r.TCut("trigger2Cut","hltRes % 1.E3 >= 1.E2"))
-
+#cutList.append(r.TCut("trigger2Cut","hltRes % 1.E3 >= 1.E2"))
 
 totalCut = r.TCut()
 for cut in cutList:
@@ -46,35 +40,43 @@ with open(dirPath+'/cuts.txt','w') as of:
 
 # Define the binnings
 binInfo = {
-    branchNames[0] : (100,2.8,3.4),
-    branchNames[1] : (100,0.,1.),
+    branchNames[0] : (100,0.0,1.0),
+    branchNames[1] : (100,2.8,3.4),
     branchNames[2] : (100,0.,1.),
-    branchNames[3] : (100,0.,1.),
-    branchNames[4] : (100,0.,2.),
-    branchNames[5] : (100,0.,2.),
-    branchNames[6] : (100,0.,0.6),
-    branchNames[7] : (100,0.,0.6),
-    branchNames[8] : (80,4.4,6.4),
-    branchNames[9] : (100,4.4,6.4),
-    branchNames[10] : (100,0.,2.),
-    branchNames[11] : (100,0.,2.),
-    branchNames[12] : (100,0.,300.),
-    branchNames[13] : (100,0.,300.),
+    branchNames[3] : (100,0.,10.),
+    branchNames[4] : (100,0.,150.),
+    branchNames[5] : (100,0.,0.6),
+    branchNames[6] : (100,-3.0,3.0),
+    branchNames[7] : (100,0.,200.),
+    branchNames[8] : (100,0.9,1.01),
+    branchNames[9] : (100,3.5,7.5),
+    branchNames[10] : (100,3.5,7.5),
+    branchNames[11] : (100,-1.0,1.0),
+    branchNames[12] : (100,-1.0,1.0),
+    branchNames[13] : (100,0.,10.),
     branchNames[14] : (100,0.,10.),
-    branchNames[15] : (100,0.,10.),
-    branchNames[16] : (100,-1.,1.),
-    branchNames[17] : (100,-1.,1.),
-    branchNames[18] : (100,0.,100.),
-    branchNames[19] : (100,0.,100.)
+    branchNames[15] : (100,-1.0,1.0),
+    branchNames[16] : (100,-1.0,1.0),
+    branchNames[17] : (100,0.,50.),
+    branchNames[18] : (100,0.,50.),
+    branchNames[19] : (100,0.,1.),
+    branchNames[20] : (100,0.,1.),
+    branchNames[21] : (100,0.,1.),
+    branchNames[22] : (100,0.,1.)
 }
+
+# For "Mod" histos, removes entries without modScale
+totalCutMod = totalCut + r.TCut("modScaleCut","modScale > 0.0")
 
 # Create and fill the histograms
 histoList = []
 for bname in branchNames[:-1]:
     binning = binInfo[bname]
     histo = r.TH1D("h"+bname,bname, binning[0],binning[1],binning[2])
-    #histo.SetAxisRange(7.e2,5.e4,"Y")
-    ntuple.Project("h"+bname,bname, str(totalCut))
+    if ("Mod" in bname):
+        ntuple.Project("h"+bname,bname, str(totalCutMod))
+    else:
+        ntuple.Project("h"+bname,bname, str(totalCut))
     histo.SetDirectory(0)
     histoList.append(histo)
 
@@ -91,7 +93,7 @@ outFile.Close()
 # Draw histograms and save the images
 for histo in histoList:
     canvas = r.TCanvas("c"+str(histo.GetTitle()))
-    canvas.SetLogy(1)
+    #canvas.SetLogy(1)
     canvas.cd()
     histo.Draw()
     canvas.Print(dirPath+'/'+str(histo.GetName())+".pdf")
