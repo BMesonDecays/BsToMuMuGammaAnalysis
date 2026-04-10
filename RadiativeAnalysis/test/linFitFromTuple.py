5084baf4c6d91efffb17e90cdc664410cfb00a1c
Pawel <p.sajdak@student.uw.edu.pl>
2026-04-10 23:26:18 +0200

    gen vs reco photon energy fitted again

--- RadiativeAnalysis/test/linFitFromTuple.py
+++ RadiativeAnalysis/test/linFitFromTuple.py
@@ -2,14 +2,14 @@
 import ROOT as r
 import sys
 
-etaMin = 0.0
-etaMax = 1.479
-xMin = 40.0
-xMax = 120.0
+etaMin = 1.65
+etaMax = 2.5
+xMin = 50.0
+xMax = 90.0
 
 tupleFileName = "genRecoPhotonEnergyCaloEta.root"
 tupleName = "tBsPhotons"
-outputName = "EB40_"
+outputName = "EES"
 
 ###########################
 tupleFile = r.TFile(tupleFileName,"READ")
@@ -17,6 +17,8 @@ ntuple = tupleFile.Get(tupleName)
 
 cutString = "TMath::Abs(eta) > "+str(etaMin)
 cutString += " && TMath::Abs(eta) < "+str(etaMax)
+#cutString += " && recoEnergy - genEnergy > -3.0"
+#cutString += " && recoEnergy - genEnergy < 4.0"
 print(cutString)
 
 newNtuple = ntuple.CopyTree(cutString)
@@ -32,19 +34,20 @@ for i in range(nPoints):
 # now fitting
 
 # Fitting function
-expression = "[0] + [1]*x"
+expression = "[0] + [1]*x "#+ [2]*x*x"
 fitFunc = r.TF1("fitFunc",expression,xMin,xMax,2)
-fitFunc.SetParameters(0.,1.)
+fitFunc.SetParameter(0,0.0)
+#fitFunc.FixParameter(1,1.0)
 
 results = graph.Fit(fitFunc,"SEMRN")
 covM = results.GetCovarianceMatrix()
 #covM.Print()
 
-funcFile = r.TFile.Open(tupleName+outputName+str(int(xMax))+"_fitFunc.root","RECREATE")
+funcFile = r.TFile.Open(tupleName+outputName+"_"+str(int(xMin))+"_"+str(int(xMax))+"_fitFunc.root","RECREATE")
 fitFunc.Write()
 funcFile.Close()
 
-with open(tupleName+outputName+str(int(xMax))+'_fitResults.txt','a') as of:
+with open(tupleName+outputName+"_"+str(int(xMin))+"_"+str(int(xMax))+'_fitResults.txt','a') as of:
     print(xMin,xMax,results,results.GetCovarianceMatrix()(0,1), file=of)
 
 # Drawing
@@ -53,8 +56,10 @@ canvas.cd()
 
 graph.SetMarkerSize(0.2)
 graph.SetMarkerStyle(20)
-graph.GetXaxis().SetRangeUser(xMin,xMax)
-graph.GetYaxis().SetRangeUser(xMin,xMax+10.0)
+graph.GetXaxis().SetRangeUser(xMin-10.0,xMax+10.0)
+graph.GetYaxis().SetRangeUser(xMin-10.0,xMax+10.0)
+#graph.GetXaxis().SetRangeUser(10.0,30.0)
+#graph.GetYaxis().SetRangeUser(5.0,40.0)
 graph.Draw("APX")
 
 r.gStyle.SetLineWidth(1)
@@ -65,23 +70,24 @@ fitFunc.SetLineStyle(1)
 fitFunc.Draw("same")
 
 # line y=x
-start = xMin
-end = xMax
+start = xMin-10.
+end = xMax+10.
 line = r.TLine(start,start,end,end)
 line.SetLineColor(4)
 line.Draw("same")
 
 
-canvas.Print(tupleName+outputName+str(int(xMax))+"_Fit.pdf")
+canvas.Print(tupleName+outputName+"_"+str(int(xMin))+"_"+str(int(xMax))+"_Fit.pdf")
 input('press enter to exit')
 
+'''
 # save just graph pdf
 c2 = r.TCanvas("c2")
 c2.cd()
-graph.GetXaxis().SetRangeUser(0,120)
-graph.GetYaxis().SetRangeUser(0,140)
-graph.Draw("AP")
-line.DrawLine(0.0,0.0,120.,120.)
+graph.GetXaxis().SetRangeUser(0,60)
+graph.GetYaxis().SetRangeUser(0,60)
+graph.Draw("APX")
+line.DrawLine(0.0,0.0,60.,60.)
 c2.Print(tupleName+outputName+".pdf")
 input('press enter to exit')
-
+'''
---
+++ RadiativeAnalysis/test/profileFromTuple.py
@@ -0,0 +1,66 @@
+#!/cvmfs/cms.cern.ch/slc7_amd64_gcc12/cms/cmssw/CMSSW_14_0_2/external/slc7_amd64_gcc12/bin/python3
+import ROOT as r
+import sys
+
+etaMin = 1.65
+etaMax = 2.5
+
+tupleFileName = "genRecoPhotonEnergyCaloEta.root"
+tupleName = "tBsPhotons"
+outputName = "EES"
+
+###########################
+tupleFile = r.TFile(tupleFileName,"READ")
+ntuple = tupleFile.Get(tupleName)
+
+cutString = "TMath::Abs(eta) > "+str(etaMin)
+cutString += " && TMath::Abs(eta) < "+str(etaMax)
+#cutString += " && recoEnergy - genEnergy > -3.0"
+#cutString += " && recoEnergy - genEnergy < 4.0"
+print(cutString)
+
+newNtuple = ntuple.CopyTree(cutString)
+nPoints = newNtuple.GetEntries()
+
+prof = r.TProfile("prof",outputName+" Photon energy;E_{gen};E_{reco}",20,10.0,80.0)
+
+
+for i in range(nPoints):
+    newNtuple.GetEntry(i)
+    prof.Fill(newNtuple.genEnergy,newNtuple.recoEnergy,1/newNtuple.recoEnergyError)
+
+
+# Drawing
+canvas = r.TCanvas("canvas")
+canvas.cd()
+
+r.gStyle.SetErrorX(0)
+#r.gStyle.SetMarkerSize(0.2)
+#r.gStyle.SetMarkerStyle(20)
+#r.gStyle.SetLineWidth(1)
+
+prof.SetStats(0)
+prof.Draw("||")
+
+# line y=x
+start = 10.
+end = 80.
+line = r.TLine(start,start,end,end)
+line.SetLineColor(4)
+line.Draw("same")
+
+
+canvas.Print(tupleName+outputName+"Profile.pdf")
+input('press enter to exit')
+
+'''
+# save just graph pdf
+c2 = r.TCanvas("c2")
+c2.cd()
+graph.GetXaxis().SetRangeUser(0,60)
+graph.GetYaxis().SetRangeUser(0,60)
+graph.Draw("APX")
+line.DrawLine(0.0,0.0,60.,60.)
+c2.Print(tupleName+outputName+".pdf")
+input('press enter to exit')
+'''
