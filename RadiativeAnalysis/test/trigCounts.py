d58dfe8c79a3ecde2818168c5da7b2eff21baad8
Pawel <p.sajdak@student.uw.edu.pl>
2026-04-26 21:33:19 +0200

    corrected HLT trig counts for JpsiGMidApril

--- RadiativeAnalysis/test/trigCounts.py
+++ RadiativeAnalysis/test/trigCounts.py
@@ -5,8 +5,8 @@ import os
 import numpy as np
 import pandas as pd
 
-filename = "./outputData/23BCD_JpsiGStartApril_cut2"
-tag = "dataCut9"
+filename = "./outputData/JpsiGMidApril_MC"
+tag = "mcCut0"
 
 # Get the tuple
 tupleFile = r.TFile(filename+".root","READ")
@@ -15,32 +15,42 @@ ntupleOrig = tupleFile.Get("tOut")
 # Define the cuts
 cutList = []
 
+cutList.append(r.TCut("dR_photonFittedDimuonCut","dR_photonFittedDimuon < 0.5 && dR_photonFittedDimuon > 0.05"))
+cutList.append(r.TCut("fittedDimuonMassCut","TMath::Abs(fittedDimuonMass - 3.097) < 0.15"))
+cutList.append(r.TCut("lXY_fittedDimuon_bSpot_sigCut","lXY_fittedDimuon_bSpot_sig > 3.0"))
+cutList.append(r.TCut("fittedDimuonVertexProbCut","fittedDimuonVertexProb > 0.01"))
+'''
 cutList.append(r.TCut("muonIdCut","muon1Id > 0.7 && muon2Id > 0.7"))
-cutList.append(r.TCut("fittedDimuonVertexProbCut","fittedDimuonVertexProb > 0.2"))
+cutList.append(r.TCut("fittedDimuonVertexProbCut","fittedDimuonVertexProb > 0.1"))
 cutList.append(r.TCut("maxMuonsVertexCompCut","maxMuonsVertexComp < 0.1"))
 cutList.append(r.TCut("fittedDimuonMassCut","TMath::Abs(fittedDimuonMass - 3.097) < 0.06"))
 cutList.append(r.TCut("lXY_fittedDimuon_bSpot_sigCut","lXY_fittedDimuon_bSpot_sig > 5.0"))
-cutList.append(r.TCut("cosAngleBsBSpot2DCut","cosAngleBsBSpot2D > 0.99998"))
-cutList.append(r.TCut("cosAnDimuonBSpot2DCut","cosAnDimuonBSpot2D > 0.98"))
 
+cutList.append(r.TCut("cosAngleBsBSpot2DCut","cosAngleBsBSpot2D > 0.99998"))
+cutList.append(r.TCut("cosAnDimuonBSpot2DCut","cosAnDimuonBSpot2D > 0.985"))
+cutList.append(r.TCut("cosAngleBsPV3DCut","cosAngleBsPV3D > 0.9999"))
+'''
 
 totalCut = r.TCut()
 for cut in cutList:
     totalCut += cut
 totalCut.Print()
 
+newFile = r.TFile("fileExtra.root","RECREATE")
 # Cut ntuple
 ntuple = ntupleOrig.CopyTree(str(totalCut))
 
 # Consider HLT paths
 triggerRes = [(int)(getattr(event, "triggerRes")) for event in ntuple]
+tupleFile.Close()
 triggerRes = ["{:08d}".format(x) for x in triggerRes]
+triggerRes = [x[1:] for x in triggerRes]    #removed first path
 
 values = [list(val) for val in triggerRes]
 
-pathNames = ['DoubleMu2_Jpsi_LowPt','DoubleMu4_3_Jpsi',\
-    'DoubleMu4_3_LowMass','DoubleMu4_JpsiTrkTrk_Displaced','DoubleMu4_Jpsi_Displaced',\
-    'DoubleMu4_Jpsi_NoVertexing','DoubleMu4_LowMass_Displaced','DoubleMu4_MuMuTrk_Displaced']
+pathNames = ['DoubleMu4_LowMass_Displaced','DoubleMu4_Jpsi_NoVertexing',\
+    'DoubleMu4_Jpsi_Displaced','DoubleMu4_JpsiTrkTrk_Displaced','DoubleMu4_3_LowMass',\
+    'DoubleMu4_3_Jpsi','DoubleMu2_Jpsi_LowPt']
 
 df = pd.DataFrame(values,columns=pathNames)
 for col in pathNames:
